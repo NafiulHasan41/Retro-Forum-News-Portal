7be51843c48b1ac3b7332622707d14a76d8901f2
Nafiul Hasan <nhashik40@gmail.com>
2024-03-04 01:36:47 +0600

    completed fetching the data from latest news api and made some section more responsive

--- assets/style.js
+++ assets/style.js
@@ -5,14 +5,58 @@ const loadDataLates = async () =>{
 
     const data = await resp.json();
 
+    const news= data;
+
    
-    displayLatesNews(data)
+    displayLatesNews(news)
 
 }
 
-const displayLatesNews= data =>{
+const displayLatesNews= news =>{
 
-    console.log(data);
+    const newsCardContainer = document.getElementById("newsCardContainer");
+    
+    news.forEach(value => {
+        
+        const lNewsCard = document.createElement("div");
+       
+
+        lNewsCard.classList=`card md:w-96 bg-base-100  shadow-xl`;
+
+        lNewsCard.innerHTML =`
+
+        <figure class="px-5 md:px-10 pt-5 md:pt-10">
+        <img src="${value.cover_image}" alt="Shoes" class="rounded-xl w-full" />
+      </figure>
+
+      <div class="card-body items-start text-start p-2 md:p-5">
+          <div>
+              <p class="text-xs"> <span class="px-2"><i class="fa-regular fa-calendar"></i></span>${value.author?.posted_date ? value.author.posted_date: "No publish date" }</p>
+          </div>
+        <h2 class="card-title text-lg text-black font-extrabold">${value.title}</h2>
+        <p class="text-sm md:text-base">${value.description}</p>
+        <div class=" mt-2 flex flex-row gap-2">
+           <div class="card-actions">
+              <img src="${value.profile_image}" alt="Shoes" class="rounded-full h-11 w-11 object-cover">
+           </div>
+
+           <div>
+              <h2 class="card-title text-sm text-black font-extrabold">${value.author?.name ? value.author.name: "Name not found"}</h2>
+              <p class="text-xs ">${value.author?.designation ? value.author.designation: "Unknown"}</p>
+           </div>
+        </div>
+      </div>
+        
+        `;
+
+        newsCardContainer.appendChild(lNewsCard);
+
+        
+    });
+
+   
+
+
 }
 
 loadDataLates();
